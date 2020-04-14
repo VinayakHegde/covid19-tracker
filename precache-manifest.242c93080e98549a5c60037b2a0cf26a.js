a95b029d89e5a840922faa7cdc26a80947960f55
Vinayak hegde <vhegde@wundermancommerce.com>
2020-04-14 23:25:58 +0100

    Updates

---
+++ precache-manifest.242c93080e98549a5c60037b2a0cf26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8008cd8d9617b606df08bb208b50cf7e",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "07bb326f38116680e853",
+    "url": "/covid19-tracker/static/css/main.62d53811.chunk.css"
+  },
+  {
+    "revision": "cd9dfe6a5f86525f66fd",
+    "url": "/covid19-tracker/static/js/2.75614296.chunk.js"
+  },
+  {
+    "revision": "07bb326f38116680e853",
+    "url": "/covid19-tracker/static/js/main.a615b5a1.chunk.js"
+  },
+  {
+    "revision": "355071e307e36d0719ef",
+    "url": "/covid19-tracker/static/js/runtime-main.a40d9b18.js"
+  },
+  {
+    "revision": "448ec07c81c7127a17ef942d28df467d",
+    "url": "/covid19-tracker/static/media/keepcalm.448ec07c.gif"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/logo.d7265326.png"
+  }
+]);
